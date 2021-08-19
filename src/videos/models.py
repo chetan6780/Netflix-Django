805552afbe7ddcd2d6a2ad44f58c9ba8192fa35d
chetan6780 <crp20802@gmail.com>
2021-08-19 23:49:17 +0530

    added publish timestamp

--- src/videos/models.py
+++ src/videos/models.py
@@ -1,9 +1,16 @@
 from django.db import models
- 
+from django.utils import timezone
 # Create your models here.
 
 
 class Video(models.Model):
+    class VideoStateOptions(models.TextChoices):
+        # CONSTANT = DB_VALUES, USER_DISPLAY_VALUES,
+        PUBLISH = 'PU','Publish'
+        DRAFT = 'DR','Draft'
+        # UNLISTED = 'UN','Unlisted'
+        # PRIVATE = 'PR','Private'
+
     title = models.CharField(max_length=220)
     description = models.TextField(blank=True,null=True)
     slug = models.SlugField(blank=True,null=True) # 'this-is-my-video'
@@ -12,11 +19,20 @@ class Video(models.Model):
     # timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
     # updated = models.DateTimeField(auto_now_add=False, auto_now=True)
     # state = models.CharField(max_length=200)
-    # publish_timestamp = models.DateTimeField(auto_now_add=False, auto_now=False)
+    state=models.CharField(max_length=2,choices=VideoStateOptions.choices ,default=VideoStateOptions.DRAFT)
+    publish_timestamp = models.DateTimeField(auto_now_add=False, auto_now=False,blank=True,null=True)
 
     @property
     def is_published(self):
         return self.active
+
+    def save(self, *args,**kwargs):
+        if self.state == self.VideoStateOptions.PUBLISH and self.publish_timestamp==None:
+            print("save as timestamp for published")
+            self.publish_timestamp = timezone.now()
+        elif self.state == self.VideoStateOptions.DRAFT:
+            self.publish_timestamp =None
+        super().save(*args,**kwargs)
 
 class VideoAllProxy(Video):
     class Meta:
